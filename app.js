368a38a78d029a35fb53e95cc229e1d58bb59b74
karyssa-dandrea <karyssadandrea@gmail.com>
2021-08-25 13:51:23 -0700

    added javascrpt

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-BaseAudioContext.code
+const nameInput = document.getElementById('name-input');
+console.log(nameInput);
+
+
+const nameValue = document.getElementById('name');
+
+const updateButton = document.getElementById('update-button');
+
+updateButton.addEventListener('click', () => {
+    const value = nameInput.value;
+    nameValue.textContent = value;
+});
